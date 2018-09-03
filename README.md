c546e1634d8ab64ed40732c2aedc56614e8d3322
Jesonhu <jesonhu_web@163.com>
2018-09-03 21:22:47 +0800

    feat: 生成html, 抽离为外部样式方式使用

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### 步骤:
+
 + `npm init -y` 快速生成 `package.json`
 + 本地安装 `webpack webpack-cli`: `npm i webpack webpack-cli -D`
 + 本地安装 `babel-core` `babel-loader`: `babel-loader` 需要安装 `babel-core`
@@ -6,8 +6,15 @@
 + 本地安装 `css-loader` `style-loader`
 + 处理scss: `node-sass sass-loader scss`
 
+### 编译
+```bash
+# webpck --mode=development
+npm run dev
+```
+
 
 ### 参考资料
 + Webpack英文官网
 + Webpack中文官网
 + Webpack4 不深不浅的实践教程
++ [webpack之loader加载顺序为啥从右往左](https://blog.csdn.net/qq_37109325/article/details/80169289)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
 
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+// 把less css scss文件分离：要使用mini-css-extract-plugin插件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: 'development',
 
@@ -9,11 +21,26 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+
+    // 关键在此项配置，需要配置为 "this", 默认为 "window"--至少在这里并没有什么用
+    // globalObject: "this"        
   },
 
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      path: path.resolve(__dirname, 'dist/style'),
+      // filename: "[name].[chunkhash:8].css",
+      filename: "main.css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
@@ -25,21 +52,44 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
-            options: { sourceMap: true }
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: 'dist'
+            }
           },
-          { loader: 'css-loader' }
+          // 如果使用 `MiniCssExtractPlugin.loader` 这里就不要使用了
+          // 使用后会报错 `webpack ReferenceError: window is not defined`
+          // {
+          //   loader: 'style-loader',
+          //   options: {
+          //     sourceMap: true
+          //   }
+          // },
+          {
+            loader: 'css-loader'
+          }
         ]
       },
       {
         test: /\.scss$/,
         use: [
-          // MiniCssExtractPlugin.loader,
-          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: 'dist'
+            }
+          },
+          // "style-loader",
           "css-loader",
           'sass-loader'
         ]
       }
     ]
-  }
+  },
+
+
 }
